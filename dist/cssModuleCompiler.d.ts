3a7c107e473734df36742f54a95cad9b47f145d8
Johan PIQUET <johan.piquet@gmail.com>
2025-08-27 21:03:02 +0200

    Untracking dist

--- dist/cssModuleCompiler.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Compile a CSS or SCSS file to a JavaScript file.
- */
-export default function compileScss(filePath: string): Promise<string>;
-export declare function scssToCss(filePath: string): any;
--- dist/esBuildPlugin.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const cssModuleHandler: Bun.OnLoadCallback;

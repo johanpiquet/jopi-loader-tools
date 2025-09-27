7d0f293a0079a610968cd2102b2b60f3be9aef0d
Johan PIQUET <johan.piquet@gmail.com>
2025-09-27 18:38:09 +0200

    Export findPackageJson

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ export { installEsBuildPlugins } from "./esBuildPlugin.ts";
 export { scssToCss } from "./cssModuleCompiler.ts";
 export { installBunJsLoader } from "./bunJsLoader.ts";
 export {searchSourceOf} from "./tools.js";
+
 export * from "./nodeJsLoader.ts";
 export * from "./config.ts";
 
+export {findExecutable, findPackageJson} from "./tools.ts"
 export * from "./binaryTools.ts"
